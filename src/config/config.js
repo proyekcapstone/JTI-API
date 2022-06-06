e65b4e14a817df4c72311354740fbbeac92a86df
Adi Munawar <munawar.adi31@gmail.com>
2022-06-06 21:14:45 +0700

    fix bugs error deploy

--- src/config/config.js
+++ src/config/config.js
@@ -6,9 +6,6 @@ dotenv.config({ path: path.join(__dirname, '../../.env') });
 
 const envVarsSchema = Joi.object()
   .keys({
-    NODE_ENV: Joi.string()
-      .valid('production', 'development', 'test')
-      .required(),
     PORT: Joi.number().default(3000),
     MONGODB_URL: Joi.string().required().description('Mongo DB url'),
     JWT_SECRET: Joi.string().required().description('JWT secret key'),
@@ -43,7 +40,6 @@ if (error) {
 }
 
 module.exports = {
-  env: envVars.NODE_ENV,
   port: envVars.PORT,
   mongoose: {
     url: envVars.MONGODB_URL + (envVars.NODE_ENV === 'test' ? '-test' : ''),
